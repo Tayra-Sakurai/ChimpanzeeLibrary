23026170312e227cfd7ea02039c93ce4451892a9
太良 桜井 <taira_sakurai@outlook.jp>
2026-01-25 20:32:19 +0900

    Total Viewer

--- ChimpanzeeLibrary/ViewModels/TayrasViewModel.cs
+++ ChimpanzeeLibrary/ViewModels/TayrasViewModel.cs
@@ -19,6 +19,15 @@ namespace ChimpanzeeLibrary.ViewModels
         [ObservableProperty]
         private ObservableCollection<Tayra> tayras;
 
+        [ObservableProperty]
+        private double cashTotal = 0;
+
+        [ObservableProperty]
+        private double icocaTotal = 0;
+
+        [ObservableProperty]
+        private double coopTotal = 0;
+
         public TayrasViewModel()
         {
             context = new ChimpanzeeContext();
@@ -35,11 +44,17 @@ namespace ChimpanzeeLibrary.ViewModels
         public async Task LoadAsync()
         {
             Tayras.Clear();
+            CashTotal = 0;
+            IcocaTotal = 0;
+            CoopTotal = 0;
             List<Tayra> tayraList = await context.Tayras.ToListAsync();
             tayraList.Sort();
             foreach (Tayra item in tayraList)
             {
                 Tayras.Add(item);
+                CashTotal += (double)item.Cash;
+                CoopTotal += (double)item.Coop;
+                IcocaTotal += (double)item.Icoca;
             }
         }
 
